f9737dc693646f3d46a23ada3cb298031144c8ec
Kate Santo <katerine.santo@gmail.com>
2020-03-10 23:40:47 +0000

    Tidying up and deploying to GH pages

--- src/findWinner.js
+++ src/findWinner.js
@@ -21,7 +21,7 @@ function findWinner(squares) {
         return null;
     }
     //WINNING COMBINATIONS
-    //     [0, 1, 2],
+    // [0, 1, 2],
     // [3, 4, 5],
     // [6, 7, 8],
     // [0, 3, 6],
